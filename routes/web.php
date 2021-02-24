e86286eb06e6fbcaaa7d9c8f03c58e5f9f06c30d
Edmund Chong Tze Wei <7440820@gmail.com>
2021-02-24 12:52:56 +1100

    modified the Readme
    Created a middleware package, hooked it with a controller, then route them all to web.php (the new route.php)

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('PackOne', [
+    "middleware" => "MiddlewarePackOne:editor",
+    "uses" => "MiddlewareControllerPackOne@index"
+]);
