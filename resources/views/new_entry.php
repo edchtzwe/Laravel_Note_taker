b9ed22d267c0c8b6f25f6b1162d909c0af0ecc2b
Edmund Chong Tze Wei <7440820@gmail.com>
2021-02-26 23:33:25 +1100

    Update_5
    -m "Introduced Angular. Life is good."

--- resources/views/new_entry.php
+++ resources/views/new_entry.php
@@ -3,7 +3,9 @@
 
 <head>
     <title>Note Entry</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
 </head>
+
 
 <body>
 
@@ -12,10 +14,29 @@
 
     <input type="hidden" name="_token" value="<?php echo csrf_token() ?>">
 
+<div ng-app="myApp" ng-controller="myController">
+
 <div>
     <label for="message">Note</label>
-    <textarea placeholder="" rows="4" cols="50" id="message" name="message" required=""></textarea>
+    <textarea rows="4" cols="50" id="message" name="message" ng-model="message" ng-change="UpdatePreview()" required=""></textarea>
 </div>
+
+<div>
+    <textarea rows="4" cols="50" id="preview" name="preview" ng-bind="preview" disabled="">{{ preview }}</textarea>
+{{ preview }}
+</div>
+
+</div>
+
+<script>
+var myApp = angular.module("myApp", []);
+myApp.controller("myController", function($scope) {
+    // $scope.preview = $scope.message;
+    $scope.UpdatePreview = function() {
+        $scope.preview = $scope.message;        
+    };
+});
+</script>
 
 <div>
     <input type="submit" value="submit"/>
